2cbde4f84138e471a98d03865d89b56fc048a794
tatyanakovalchuk <72554880+tatyanakovalchuk@users.noreply.github.com>
2020-11-10 14:29:45 +0200

    Update БД телефонной книги.py

--- БД телефонной книги.py
+++ БД телефонной книги.py
@@ -2,13 +2,14 @@
 import sys
 import sqlite3
 
-
-def find_note():
-    pass
 def edit_note():
     pass
 def delete_note():
     pass
+def displaybook():
+    cur.execute("SELECT surname, name, phone_number FROM phonebook ORDER BY surname")
+    results = cur.fetchall()
+    print(results)
 def close_win():
     print("Выполнен выход из программы")
     sys.exit(0)
@@ -19,7 +20,8 @@ def print_menu():
     print("2. Найти запись")
     print("3. Редактировать запись")
     print("4. Удалить запись")
-    print("5. Выйти из программы")
+    print("5. Показать все записи")
+    print("6. Выйти из программы")
 
 
 conn = sqlite3.connect('phonebook.db')
@@ -34,18 +36,20 @@ cur.execute("""CREATE TABLE IF NOT EXISTS phonebook(
 conn.commit()
 
 menu_choice = 0
-while menu_choice != 5:
+while menu_choice != 6:
     print_menu()
-    menu_choice = int(input("Введите число от 1 до 5:"))
+    menu_choice = int(input("Введите число от 1 до 6:"))
     if menu_choice == 1:
         import createmodule
     elif menu_choice == 2:
-        find_note()
+        import find_module
     elif menu_choice == 3:
         edit_note()
     elif menu_choice == 4:
         delete_note()
     elif menu_choice == 5:
+        displaybook()
+    elif menu_choice == 6:
         close_win()
     else:
         print("Пожалуйста следуйте инструкциям")
